b99ac8ab6ebd48d6bc344cfb201fa0503a7dfaf6
Hanmin Kim <hanminkim123@gmail.com>
2024-12-26 02:45:30 -0500

    feat(span): add more span features (#5)
    * make spans cloneable
    
    * add dummy span
    
    * add join spans

--- compiler/bfc_span/src/span.rs
+++ compiler/bfc_span/src/span.rs
@@ -1,7 +1,30 @@
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 pub struct BytePos(pub u32);
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 pub struct Span {
     pub lo: BytePos,
     pub hi: BytePos,
+}
+
+pub const DUMMY_SPAN: Span = Span {
+    lo: BytePos(0),
+    hi: BytePos(0),
+};
+
+pub fn join_spans<'a, I>(spans: I) -> Span
+where
+    I: Iterator<Item = &'a Span>,
+{
+    let mut lo = 0;
+    let mut hi = 0;
+
+    for span in spans {
+        lo = lo.min(span.lo.0);
+        hi = hi.max(span.hi.0);
+    }
+
+    Span {
+        lo: BytePos(lo),
+        hi: BytePos(hi),
+    }
 }
