4339d673881701a20fb4a293777554827830f70d
Hanmin <hanminkim123@gmail.com>
2024-12-25 22:08:37 -0800

    make spans cloneable

--- compiler/bfc_span/src/span.rs
+++ compiler/bfc_span/src/span.rs
@@ -1,6 +1,6 @@
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 pub struct BytePos(pub u32);
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 pub struct Span {
     pub lo: BytePos,
     pub hi: BytePos,
